9a89f46f180d8126d413db81ed922bacf911ca83
Sana Afzal <94588438+sanuafzal@users.noreply.github.com>
2023-12-08 18:29:02 +0500

    Update Groceries.js

--- Groceries.js
+++ Groceries.js
@@ -1,56 +1,123 @@
-// Scenario 1 - Buying Groceries
-function buyGroceries(choice) {
-    let totalBill = 0;
-    if (choice === 'fruits') {
-        console.log('Available fruits: Apple, Banana, Orange');
-        const fruitChoice = 'Apple'; // Assume the user chooses Apple
-        const fruitPrice = 2.5;
-        totalBill += fruitPrice;
-        console.log(`Added ${fruitChoice} to your cart. Price: $${fruitPrice}`);
-    }
-    else if (choice === 'vegetables') {
-        console.log('Available vegetables: Carrot, Broccoli, Spinach');
-        const vegChoice = 'Carrot'; // Assume the user chooses Carrot
-        const vegPrice = 1.5;
-        totalBill += vegPrice;
-        console.log(`Added ${vegChoice} to your cart. Price: $${vegPrice}`);
-    }
-    else {
-        console.log('Invalid choice. Please choose fruits or vegetables.');
-    }
-    return totalBill;
-}
-// Scenario 2 - Checking Discounts
-function applyDiscount(totalBill) {
-    let discountedTotal = totalBill;
-    if (totalBill > 10) {
-        const discountPercentage = 0.1; // 10% discount for bills above $10
-        discountedTotal = totalBill - totalBill * discountPercentage;
-        console.log(`You get a 10% discount! New total: $${discountedTotal}`);
-    }
-    return discountedTotal;
-}
-// Scenario 3 - Checkout Process
-function checkoutProcess(paymentMethod) {
-    const paymentOptions = ['Credit Card', 'Debit Card', 'Cash'];
-    let isValidPayment = false;
-    for (let i = 0; i < paymentOptions.length; i++) {
-        console.log(`[${i + 1}] ${paymentOptions[i]}`);
-    }
-    while (!isValidPayment) {
-        const selectedOption = 2; // Assume the user chooses Debit Card
-        if (selectedOption >= 1 && selectedOption <= paymentOptions.length) {
-            console.log(`You have chosen ${paymentOptions[selectedOption - 1]} for payment. Thank you!`);
-            isValidPayment = true;
+
+import inquirer from 'inquirer';
+const fruits = [
+    { name: 'Apple', price: 2 },
+    { name: 'Banana', price: 1 },
+    { name: 'Orange', price: 3 },
+];
+const vegetables = [
+    { name: 'Tomato', price: 1 },
+    { name: 'Carrot', price: 2 },
+    { name: 'Potato', price: 0.5 },
+];
+const groceries = [
+    { name: 'Milk', price: 3 },
+    { name: 'Bread', price: 2 },
+    { name: 'Eggs', price: 4 },
+];
+let cart = [];
+let totalBill = 0;
+function startShopping() {
+    inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'department',
+            message: 'What department would you like to visit?',
+            choices: ['Fruits', 'Vegetables', 'Groceries'],
+        },
+    ])
+        .then((answers) => {
+        if (answers.department === 'Fruits') {
+            console.log('welocome to the fruits market');
+            buyFruits();
+        }
+        else if (answers.department === 'Vegetables') {
+            console.log('welocome to the Vegetables');
+            buyVegetables();
         }
         else {
-            console.log('Invalid option. Please choose a valid payment method.');
+            buyGroceries();
+            console.log('welocome to the buying Groceries');
         }
-    }
+    });
 }
-// Main shopping simulation
-const groceryChoice = 'fruits'; // Assume the user wants to buy fruits
-const totalBill = buyGroceries(groceryChoice);
-const discountedTotal = applyDiscount(totalBill);
-checkoutProcess('Debit Card'); // Assume the user chooses Debit Card for payment
-export {};
+function buyFruits() {
+    inquirer
+        .prompt([
+        {
+            type: 'checkbox',
+            name: 'fruits',
+            message: 'Select the fruits you want to buy:',
+            choices: fruits.map((fruit) => fruit.name),
+        },
+    ])
+        .then((answers) => {
+        for (const fruit of fruits) {
+            if (answers.fruits.includes(fruit.name)) {
+                cart.push(fruit);
+                totalBill += fruit.price;
+            }
+        }
+        checkDiscounts();
+    });
+}
+function buyVegetables() {
+    inquirer
+        .prompt([
+        {
+            type: 'checkbox',
+            name: 'vegetables',
+            message: 'Select the vegetables you want to buy:',
+            choices: vegetables.map((vegetables) => vegetables.name),
+        },
+    ])
+        .then((answers) => {
+        for (const vegetable of vegetables) {
+            if (answers.vegetables.includes(vegetable.name)) {
+                cart.push(vegetable);
+                totalBill += vegetable.price;
+            }
+        }
+        checkDiscounts();
+    });
+}
+function buyGroceries() {
+    inquirer
+        .prompt([
+        {
+            type: 'checkbox',
+            name: 'Groceries',
+            message: 'Select the Groceries you want to buy:',
+            choices: groceries.map((groceries) => groceries.name),
+        },
+    ])
+        .then((answers) => {
+        for (const grocery of groceries) {
+            if (answers.groceries.includes(grocery.name)) {
+                cart.push(grocery);
+                totalBill += grocery.price;
+            }
+        }
+        checkDiscounts();
+    });
+}
+function checkDiscounts() {
+    if (totalBill > 10) {
+        totalBill *= 0.9; // 10% discount
+    }
+    inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'paymentMethod',
+            message: 'How would you like to pay?',
+            choices: ['Cash', 'Credit Card', 'Debit Card'],
+        },
+    ])
+        .then((answers) => {
+        console.log('Your total bill is:', totalBill.toFixed(2));
+        console.log(`Payment method: ${answers.paymentMethod}`);
+    });
+}
+startShopping();
